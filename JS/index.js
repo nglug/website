14d91980c9715aa738209593626c36fc80bbb376
will <86805234+nglug@users.noreply.github.com>
2024-02-29 23:28:54 +0000

    counter

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,34 @@
 var x = 0
-    function buttonClick() {
-    if (x == 0) {
-        document.getElementById("navbar").style.top = "0px";
-        x ++;
-        document.getElementById("main").style.marginTop = "38px";
 
-    } else {
-        document.getElementById("navbar").style.top = "-50px";
-        x --;
-        document.getElementById("main").style.marginTop = "0px";
-    }
-    
-    }
+function buttonClick() {
+if (x == 0) {
+    document.getElementById("navbar").style.top = "0px";
+    x ++;
+    document.getElementById("main").style.marginTop = "50px";
+
+
+} else {
+    document.getElementById("navbar").style.top = "-50px";
+    x --;
+    document.getElementById("main").style.marginTop = "0px";
+
+}
+
+}
+
+function change() {
+  if (document.getElementById("number").innerHTML < 0) {
+    document.getElementById("number").style.color = "red"
+  } else if (document.getElementById("number").innerHTML > 0) {
+    document.getElementById("number").style.color = "green"
+  } else {
+    document.getElementById("number").style.color = "grey"
+  }
+}
+
+function buttonUp() {
+  document.getElementById("number").innerHTML = Number(document.getElementById("number").innerHTML) + 1
+}
+function buttonDown() {
+  document.getElementById("number").innerHTML = Number(document.getElementById("number").innerHTML) - 1
+}
