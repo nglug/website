7421d8a1a2d329b82d796f28f9e37969d416d694
will <86805234+nglug@users.noreply.github.com>
2024-02-29 19:41:50 +0000

    fix

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,14 @@
-console.log("loaded JS")
+var x = 0
+    function buttonClick() {
+    if (x == 0) {
+        document.getElementById("navbar").style.top = "0px";
+        x ++;
+        document.getElementById("main").style.marginTop = "38px";
+
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+        x --;
+        document.getElementById("main").style.marginTop = "0px";
+    }
+    
+    }
